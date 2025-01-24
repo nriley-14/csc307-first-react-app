85c964d9d478152d5714c7e0db4d3aefdfd96ba4
Nick Riley <nick@rileyorchards.com>
2025-01-24 09:30:14 -0800

    Allow frontend to post to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
+
   
 function MyApp() {
     const [characters, setCharacters] = useState([]);
@@ -14,6 +15,36 @@ function MyApp() {
     function updateList(person) {
         setCharacters([...characters, person]);
     }
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(person)
+        });
+      
+        return promise;
+    }
+    function updateList(person) {
+        postUser(person)
+          .then(() => setCharacters([...characters, person]))
+          .catch((error) => {
+            console.log(error);
+        });
+    }
+    useEffect(() => {
+        fetchUsers()
+          .then((res) => res.json())
+          .then((json) => setCharacters(json["users_list"]))
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
     return (
         <div className="container">
             <Table
